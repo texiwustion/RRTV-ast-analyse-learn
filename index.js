d6e54fca4b87dfc443ce21ecc6527051c2a8ec8d
texiwustion <w6666s@outlook.com>
2023-06-24 16:48:07 +0800

    🎈 perf: 更改命名

--- index.js
+++ index.js
@@ -1,38 +1,16 @@
-// → 导入模块
-const parser = require('@babel/parser');
-const traverse = require('@babel/traverse')
-// → 定义一段代码字符串
-const code = `
-        function getName() {
-            return <div>
-                your name
-            </div>
-        } 
-        function getAge() {
-            const age = 14
-            return <div>
-                my age is {age}
-            </div>
+module.exports = ({ types: t }) => ({
+    /** 插件名称 */
+    name: 'babel-plugin-1',
+    /** 访问者 */
+    visitor: {
+        // coding in here...
+        BinaryExpression(path) {
+            if (path.node.operator !== "===") {
+                return;
+            }
+
+            path.node.left = t.identifier("sebmck");
+            path.node.right = t.identifier("dork");
         }
-    `;
-// → 解析代码字符串
-const ast = parser.parse(code, {
-    sourceType: 'script', // module unambigious
-    plugins: ['jsx', 'typescript'],
+    },
 });
-
-// visitor
-
-const visitor = {
-    JSXElement(path) {
-        console.log('jsx!')
-        console.log(path.node)
-    }
-}
-
-// traverse
-
-traverse.default(ast, visitor)
-
-// → 输出ast
-// console.log(JSON.stringify(ast, null, 4));
